1fdbcded67e46c5ce85cae2ce6c589924e09ac75
Belhassen <Belhassen.Gharsallah@isetrades.com>
2018-08-25 15:12:50 +0200

    String Reveral Solution 2

--- src/Algorithms/string/string-reversal/stringReversal.js
+++ src/Algorithms/string/string-reversal/stringReversal.js
@@ -1,7 +1,21 @@
+/* Solution: 1 */
+/*
 const reverse = (string) => (
   string.length > 2 ?
   string.split('').reverse().join('') :
   string === undefined ? 'put your string' : 'damn must be more then two character!';
 );
 
-console.log(reverse(''));
+*/
+
+/* Solution: 2 */
+
+const reverse = (string) => {
+  let reversedString = '';
+  for (let char of string) {
+    reversedString = char + reversedString;
+  }
+  return reversedString;
+}
+
+console.log(reverse('ABC'));
