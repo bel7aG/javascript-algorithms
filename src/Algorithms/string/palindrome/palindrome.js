42bfe012604b6291c62e6fa06c382fef787c0f48
Belhassen <Belhassen.Gharsallah@isetrades.com>
2018-08-25 16:51:37 +0200

    Palindrome Solution 1 my Bad ahahahah

---
+++ src/Algorithms/string/palindrome/palindrome.js
@@ -0,0 +1,19 @@
+/* Solution: 1 */
+let palindrome = (current) => {
+  if (!current.length) {
+    return `EMPTY!`;
+  }
+  if (current.length === 1) {
+    return `Obviously it is !! haHaHaha`;
+  }
+
+  let revStr = '';
+  for (let i = current.length; i !== 0; i--) {
+    revStr = revStr + current[i];
+  }
+  return current === revStr ? 'Palindrom' : 'not Palindrome';
+};
+
+console.log(palindrome('radar'));
+console.log(palindrome(''));
+console.log(palindrome('z'));
