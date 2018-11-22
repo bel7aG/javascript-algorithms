0131758fe9f9976a99ae3857c4bbbf0ad14c3ed3
bel7aG <belhassen.gharsallah@isetrades.com>
2018-11-22 12:53:50 +0100

    char counts method two with regex

--- src/Algorithms/string/char-counts/charCounts.js
+++ src/Algorithms/string/char-counts/charCounts.js
@@ -1,10 +1,9 @@
-const charCounts = (str) => {
-  if (str.length) {
-
+/* Method 1: no matter if it is a lower or upper cases and Numbers */
+const charCountsOne = (str) => {
+  if (str) {
     let obj = {}, char, isOkey
-
     for (let i = 0; i < str.length; i++) {
-      char = str[i].toLowerCase()
+      char = str[i]
       isOkey = (
           (char.charCodeAt() >= 48 && char.charCodeAt() <= 57) ||
           (char.charCodeAt() >= 65 && char.charCodeAt() <= 90) ||
@@ -21,7 +20,29 @@ const charCounts = (str) => {
   return `Empty String`
 }
 
-console.log(charCounts(`Belhassen &&&& * $ Gharsallah     # ' " bel7aG OH YEAH`))
+console.log(charCountsOne(`Belhassen &&&& * $ Gharsallah     # ' " bel7aG OH YEAH`))
+
+console.log('\n\n\n\n')
+
+//Method 2: just the lower cases and Numbers
+
+const charCountsTwo = (str) => {
+  if (str) {
+    let obj = {}, char
+    for (let i = 0; i < str.length; i++) {
+      char = str[i].toLowerCase()
+      if (obj[char] > 0) {
+        obj[char]++
+      } else if (char.match(/[a-z0-9]/)){
+        obj[char] = 1
+      }
+    }
+    return obj
+  }
+  return `Empty String`
+}
+
+console.log(charCountsTwo(`Belhassen &&&& * $ Gharsallah     # ' " bel7aG OH YEAH`))
 
 /*  Syntax    */
 
