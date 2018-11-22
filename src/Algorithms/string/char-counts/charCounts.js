f036264444d36163dd72e2cc06048a38be06e3ad
bel7aG <belhassen.gharsallah@isetrades.com>
2018-11-22 12:07:53 +0100

    charCounts version 0.0.1

--- src/Algorithms/string/char-counts/charCounts.js
+++ src/Algorithms/string/char-counts/charCounts.js
@@ -1,14 +1,26 @@
 const charCounts = (str) => {
-  let obj = {}, char
-  for (let i = 0; i < str.length; i++) {
-    char = str[i].toLowerCase()
-    if (obj[char] > 0) {
-      obj[char]++
-    } else {
-      obj[char] = 1
+  if (str.length) {
+    let obj = {}, char
+    for (let i = 0; i < str.length; i++) {
+      char = str[i].toLowerCase()
+      if (obj[char] > 0) {
+        obj[char]++
+      } else {
+        obj[char] = 1
+      }
     }
+    return obj
   }
-  return obj
+  return `Empty String`
 }
 
-console.log(charCounts("Belhassen Gharsallah"))
+console.log(charCounts(`Belhassen Gharsallah bel7aG OH YEAH`))
+
+/*  Syntax    */
+
+let x = {
+  a: 2,
+  b: 33
+}
+
+console.log(`\n\n\t\t\tx['a'] === x.a : ${x['a'] === x.a} `) //true
